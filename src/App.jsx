269f9755a73bf7c91943773ecc379bae73769581
Kristjan Lääne <xonproject@gmail.com>
2023-09-28 19:00:41 +0300

    matched prop

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,12 @@ import './App.css'
 import { SingleCard } from './components/SingleCard'
 
 const cardImages = [
-  { src: '/img/helmet-1.png' },
-  { src: '/img/potion-1.png' },
-  { src: '/img/ring-1.png' },
-  { src: '/img/scroll-1.png' },
-  { src: '/img/shield-1.png' },
-  { src: '/img/sword-1.png' },
+  { src: '/img/helmet-1.png', matched: false },
+  { src: '/img/potion-1.png', matched: false },
+  { src: '/img/ring-1.png', matched: false },
+  { src: '/img/scroll-1.png', matched: false },
+  { src: '/img/shield-1.png', matched: false },
+  { src: '/img/sword-1.png', matched: false },
 ]
 
 function App() {
@@ -37,14 +37,23 @@ function App() {
       console.log(choiceTwo)
 
       if (choiceOne.img === choiceTwo.img) {
-        console.log('match!')
+        setCards((prevCards) => {
+          return prevCards.map((card) => {
+            if (card.src === choiceOne.img) {
+              return { ...card, matched: true }
+            } else {
+              return card
+            }
+          })
+        })
         resetTurn()
       } else {
-        console.log('no match!')
         resetTurn()
       }
     }
   }, [choiceOne, choiceTwo])
+
+  console.log(cards)
 
   const resetTurn = () => {
     setChoiceOne(null)
